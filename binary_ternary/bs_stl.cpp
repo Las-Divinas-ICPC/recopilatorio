1666aa1a2eb02208ba0cdf14ba393c7786699a18
Lord Friky <24846044+lordfriky@users.noreply.github.com>
2023-05-22 03:40:14 -0600

    Update bs_stl.cpp

--- binary_ternary/bs_stl.cpp
+++ binary_ternary/bs_stl.cpp
@@ -1,5 +1,6 @@
 // Primer not < (primer elemento o mayor)
-lower_bound(Iterator first, Iterator last, Find x);
+Iterator lower_bound(Iterator first, Iterator last, Find x);
 // Primer > (elemento siguente del último)
-upper_bound(Iterator first, Iterator last, Find x);
+Iterator upper_bound(Iterator first, Iterator last, Find x);
 // @end si no se encontró en ambos casos
+bool binary_search(Iterator first, Iterator last, Find x);
